6e4850be2db857b864e99d76d25e1e4192e38c15
Diego Figueroa <diegofigueroa99@gmail.com>
2014-07-23 09:50:34 -0600

    Update strobe.js

--- strobe.js
+++ strobe.js
@@ -6,8 +6,8 @@ var board = new five.Board({
 });
 
 board.on("ready", function() {
-  // Create an Led on pin 13
-  var led = new five.Led(13);
+  // Create an Led on pin 7 (GPIO4)
+  var led = new five.Led(7);
   
   // Strobe the pin on/off, defaults to 100ms phases
   led.strobe();
